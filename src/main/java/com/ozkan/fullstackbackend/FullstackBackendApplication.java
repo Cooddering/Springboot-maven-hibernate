45b46eb54346645ccc610f9e6515f0d7f9d4f979
özkan <46302195+Cooddering@users.noreply.github.com>
2022-10-24 00:38:54 +0300

    first

---
+++ src/main/java/com/ozkan/fullstackbackend/FullstackBackendApplication.java
@@ -0,0 +1,15 @@
+package com.ozkan.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ozkan/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ozkan.fullstackbackend.repository;
+
+import com.ozkan.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
